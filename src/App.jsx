3a0163e1734207903602a5868fbd2d4ebf4d1e7d
trungshin <trungviet6969@gmail.com>
2024-03-18 00:13:13 +0700

    Light dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 import Button from '@mui/material/Button'
+import { useColorScheme } from '@mui/material/styles'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
 
 function App() {
   return (
     <>
+      <ModeToggle />
+      <hr />
       <div>trungshin</div>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
