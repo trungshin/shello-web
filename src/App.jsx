6c98639c163cde06e505a005858ea93bd1b83f27
trungshin <trungviet6969@gmail.com>
2024-03-18 10:57:57 +0700

    Sync dark light mode with system

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
 import Button from '@mui/material/Button'
 import { useColorScheme } from '@mui/material/styles'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
 
-function ModeToggle() {
+function SelectMode() {
+  const { mode, setMode } = useColorScheme()
+
+  const handleChange = (e) => {
+    const selectedMode = e.target.value
+    setMode(selectedMode)
+  }
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
+      <Select
+        labelId="label-select-dark-light-mode"
+        id="select-dark-light-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value="light">Light</MenuItem>
+        <MenuItem value="dark">Dark</MenuItem>
+        <MenuItem value="system">System</MenuItem>
+      </Select>
+    </FormControl>
+  )
+}
+
+function ToggleMode() {
   const { mode, setMode } = useColorScheme()
   return (
     <Button
@@ -17,7 +47,9 @@ function ModeToggle() {
 function App() {
   return (
     <>
-      <ModeToggle />
+      <SelectMode />
+      <hr />
+      <ToggleMode />
       <hr />
       <div>trungshin</div>
       <Button variant="contained">Contained</Button>
