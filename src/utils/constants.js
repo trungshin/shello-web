5a75f5e5a5cc3603380ac4ba8d310c0d7214d613
trungshin <trungviet6969@gmail.com>
2024-09-20 23:06:30 +0700

    Fix BUILD_MODE dev and prod

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,10 @@
-export const API_ROOT = 'http://localhost:5000'
+let apiRoot = ''
+if (process.env.BUILD_MODE === 'dev') {
+  apiRoot = 'http://localhost:5000'
+}
+
+if (process.env.BUILD_MODE === 'prod') {
+  apiRoot = 'https://shello-api-vrtr.onrender.com'
+}
+
+export const API_ROOT = apiRoot
